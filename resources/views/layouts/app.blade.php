1c4d95253904231bde4f5f25a4fba34d7b521924
christian <danicroosing@gmail.com>
2021-01-12 04:15:09 +0100

    Your message about the commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by earnpointe</title>
+
+    <!-- Font Icon -->
+
+<link rel="stylesheet" href="{{ asset('fonts/material-icon/css/material-design-iconic-font.min.css') }}">
+
+    <!-- Main css -->
+    
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+   
+</head>
+<body>
+    <div class="main">
+        @yield('content')
+    </div>
+ <!-- JS -->
+ <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
